6b322681489ccb9f9c53e92254d74de38dfb4f89
forkingits1123 <lblack11235813@gmail.com>
2021-05-17 11:32:02 -0700

    commit tree 1d2ed9e43863691eac90364c4532508f3f42d8a6

--- index.js
+++ index.js
@@ -1,1 +1,5 @@
-// Write your code in this file!
+const currentUser = 'Rufus Spunkmeyer';
+const welcomeMessage = `Welcome to Flatbook, ${currentUser}!`;
+const excitedWelcomeMessage = welcomeMessage.toUpperCase();
+const firstLetter = currentUser[0];
+const shortGreeting = `Welcome, ${firstLetter}!`;
